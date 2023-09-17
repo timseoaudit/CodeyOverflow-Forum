739c8a57c172f1d2cc68c15499d923164224f91d
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-09-17 04:20:28 +0100

    add body and header function start card

--- files/App.js
+++ files/App.js
@@ -1,2 +1,7 @@
 import React from 'react';
-import {comments} from './commentData'
+import {comments} from './commentData';
+import Card from './Card';
+
+function App(comments) {
+    comments.map(comment => <Card commentObject={comment} /> )
+}
--- files/Card.js
+++ files/Card.js
@@ -1,1 +1,12 @@
 import React from 'react';
+import Header from './Header';
+import Body from './Body';
+
+function Card(props) {
+    return (
+        <Header />
+        <Body />
+    );
+};
+
+export default Card;
